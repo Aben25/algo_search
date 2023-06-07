2b40075e9f042b9cd154c18fccb264aadd95fa67
Abenezer Nuro <anuro@artba.org>
2023-06-07 10:07:21 -0400

    More filter

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import {
   InstantSearch,
   Configure,
   RefinementList,
-  InfiniteHits,
+  connectRefinementList,
   connectStateResults,
   SearchBox,
   connectHits,
@@ -23,45 +23,78 @@ const searchClient = algoliasearch("6V4U26IN4K", "20e488ed9f87b0b54e36cb36667512
 const Hits = connectHits(({ hits }) => (
   <div className="bg-white shadow overflow-hidden sm:rounded-lg mb-2">
     {hits.map(hit => (
-      <a href={hit.permalink} target="_blank" rel="noopener noreferrer">
-        <div className="px-4 py-5 sm:px-6" key={hit.objectID}>
+      <div className="px-4 py-5 sm:px-6" key={hit.objectID}>
+        <a href={hit.permalink} target="_blank" rel="noopener noreferrer">
+
           <h3 className="text-lg leading-6 font-medium text-gray-900">
             {hit.post_title}  {/* assuming "title" is a field in your Algolia index data */}
           </h3>
-          <p className="mt-1 max-w-2xl text-sm text-gray-500">
-            {hit.post_excerpt} {/* assuming "excerpt" is a field in your Algolia index data */}
-          </p>
+        </a>
 
-        </div>
-      </a>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">
+          {/* Onle show few sentense from hit.content */}
+          {hit.content.substring(0, 150)}... {/* assuming "excerpt" is a field in your Algolia index data */}
+        </p>
+
+      </div>
     ))}
   </div>
 ));
 
 // Custom Results component that only shows hits when a query has been made
 const Results = connectStateResults(
-  ({ searchState }) => searchState && searchState.query
-    ? <Hits />
-    : null
+  ({ searchState }) => 
+     <Hits />
+    
 );
+
+const CustomRefinementList = ({ items, refine }) => (
+  <ul>
+    {items.map(item => {
+      const url = new URL(item.label);
+      const base = url.hostname;
+      return (
+        <li key={item.label}>
+          <input
+            type="checkbox"
+            checked={item.isRefined}
+            onChange={() => refine(item.value)}
+          />
+          <label>{base}</label>
+        </li>
+      );
+    })}
+  </ul>
+);
+const ConnectedRefinementList = connectRefinementList(CustomRefinementList);
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8 mb-0 flex">
       <InstantSearch searchClient={searchClient} indexName="artba_searchable_posts">
-        <Configure hitsPerPage={10} />
+        <Configure hitsPerPage={3} />
         <div className="w-1/4 p-4">
           <h2 className="text-lg leading-7 font-medium text-gray-900">Filter</h2>
           <hr className="mt-2 mb-2" />
           <h5>Post Type</h5>
           <RefinementList attribute="post_type" />
+          <h5>Category</h5>
+          <RefinementList attribute="taxonomies.category" />
+          <h5>Permalink</h5>
+          <ConnectedRefinementList attribute="permalink" />
         </div>
         <div className="w-3/4 p-4">
           <SearchBox />
-          <Index indexName="tdf_searchable_posts">
+          <Index indexName="artba_searchable_posts">
+            <h2>ARTBA</h2>
             <Results />
           </Index>
-          <Index indexName="artba_searchable_posts">
+          <Index indexName="Newsline_searchable_posts">
+            <h2>Newsline</h2>
+            <Results />
+          </Index>
+          <Index indexName="tdf_searchable_posts">
+            <h2>TDF</h2>
             <Results />
           </Index>
         </div>
